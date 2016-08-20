699c6dace22f10761eb9e0ec530307fc8a3cc18d
Researchnix <forschnix@gmail.com>
2016-08-20 11:47:38 -0400

    "update"

---
+++ Makefile
@@ -0,0 +1,14 @@
+#
+#  Makefile
+#  6nimmt
+#
+#  Created by Lennart Doppenschmitt on 2016-07-01.
+#  Copyright 2016 Researchnix. All rights reserved.
+#
+
+output:
+	clear
+	python main.py
+
+clean:
+	rm -rf *.pyc
---
+++ main.py
@@ -0,0 +1,19 @@
+#
+#  main.py
+#  HackThe6ix
+#
+#  Created by Jeffrey, Maliha and Lennart on 2016-08-06.
+#  Copyright 2016 Researchnix. All rights reserved.
+#
+
+import sys
+import time
+
+
+
+
+if __name__ == "__main__":
+    t = time.time()
+
+
+    print "\n\nDone in " + str(time.time() - t) + " s"
