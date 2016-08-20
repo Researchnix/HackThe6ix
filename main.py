3d8a80cc5d46d56961eaab9b802bf30d9697b59b
Researchnix <forschnix@gmail.com>
2016-08-20 14:37:34 -0400

    "update"

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #  main.py
 #  HackThe6ix
 #
-#  Created by Jeffrey, Maliha and Lennart on 2016-08-06.
+#  Created by Jeffrey, Maliha, Justin  and Lennart on 2016-08-06.
 #  Copyright 2016 Researchnix. All rights reserved.
 #
 
